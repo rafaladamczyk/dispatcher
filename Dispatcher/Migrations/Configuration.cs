7fbc60117f20b886bd1209ee38719e29f4add28d
Rafal Adamczyk <rafal.adamczyk@grantadesign.com>
2016-04-02 20:46:24 +0200

    oops, restore missing roles in Seed method

--- Dispatcher/Migrations/Configuration.cs
+++ Dispatcher/Migrations/Configuration.cs
@@ -1,22 +1,33 @@
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
 using Dispatcher.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Dispatcher.Migrations
 {
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
-    internal sealed class Configuration : DbMigrationsConfiguration<Dispatcher.Models.DispatcherContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DispatcherContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Dispatcher.Models.DispatcherContext context)
+        protected override void Seed(DispatcherContext context)
         {
             AddUser(context, "Admin", "admin@123", "Admin");
+            AddRole(context, "TworzenieZlecen");
+            AddRole(context, "ObslugaZlecen");
+        }
+
+        private void AddRole(DbContext context, string role)
+        {
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+            var newRole = new IdentityRole { Name = role };
+
+            roleManager.Create(newRole);
         }
 
         private void AddUser(DispatcherContext context, string userName, string password, params string[] roles)
