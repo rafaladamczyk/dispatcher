7d6a1b8fab9f20b648cc87ba52489eb42398218c
Rafal Adamczyk <rafal.adamczyk@grantadesign.com>
2017-04-22 11:26:01 +0200

    added periodic removal of old  requests in progress

--- Dispatcher/Global.asax.cs
+++ Dispatcher/Global.asax.cs
@@ -1,12 +1,21 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Dispatcher.Models;
+using Microsoft.AspNet.SignalR;
 
 namespace Dispatcher
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -14,6 +23,58 @@ namespace Dispatcher
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            try
+            {
+                new Task(
+                    async () =>
+                    {
+                        while (true)
+                        {
+                            IDispatcherContext db = DispatcherContext.Create();
+                            
+                            try
+                            {
+                                DateTime expiryDate = DateTime.UtcNow - TimeSpan.FromHours(1);
+                                var overdueRequests =
+                                    db.Requests.Where(
+                                        r =>
+                                            r.Active && r.PickedUpDate.HasValue && !r.Type.ForSelf
+                                            && DbFunctions.CreateDateTime(
+                                                r.PickedUpDate.Value.Year,
+                                                r.PickedUpDate.Value.Month,
+                                                r.PickedUpDate.Value.Day,
+                                                r.PickedUpDate.Value.Hour,
+                                                r.PickedUpDate.Value.Minute,
+                                                r.PickedUpDate.Value.Second)
+                                            < DbFunctions.CreateDateTime(expiryDate.Year, expiryDate.Month, expiryDate.Day, expiryDate.Hour, expiryDate.Minute, expiryDate.Second));
+                                
+                                foreach (var request in overdueRequests)
+                                {
+                                    request.PickedUpDate = null;
+                                    request.ProvidingUserName = null;
+                                }
+
+                                db.SaveChanges();
+                                var activeRequests = db.Requests.Where(r => r.Active).ToList();
+                                GlobalHost.ConnectionManager.GetHubContext<RequestsHub>().Clients.All.updateActiveRequests(activeRequests);
+                                await Task.Delay(TimeSpan.FromMinutes(5), cancellationTokenSource.Token);
+                            }
+                            catch
+                            {
+                            }
+                        }
+                    },cancellationTokenSource.Token,
+                    TaskCreationOptions.LongRunning).Start();
+            }
+            catch (TaskCanceledException)
+            {
+            }
+        }
+
+        protected void Application_End()
+        {
+            cancellationTokenSource.Cancel();
         }
     }
 }
